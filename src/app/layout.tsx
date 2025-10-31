c0b13c4c739da5049e8216f1cd4a7e026a564cb1
geovla <geovla.dev@gmail.com>
2025-10-31 12:10:29 +0200

    Add @vercel/analytics dependency and integrate Analytics component in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -43,6 +44,7 @@ export default function RootLayout({
           <Footer className="mt-20" />
         </ThemeProvider>
         <Toaster richColors />
+        <Analytics />
       </body>
     </html>
   );
