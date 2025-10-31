3e31e7f07ee298dc69bd392eeabd43e0be74b1ab
geovla <geovla.dev@gmail.com>
2025-10-31 12:51:02 +0200

    Update sitemap and robots configuration to use environment variable for site URL

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://geovla.dev/sitemap.xml",
+    sitemap: `${process.env.NEXT_PUBLIC_SITE_URL}/sitemap.xml`,
   };
 }
